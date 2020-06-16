d5bcbce8022e9e8bf50da5405d05d47e17f0260d
dpberry552 <dpberry178@gmail.com>
2020-06-15 22:09:08 -0400

    first mockup of timer component

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  hours = this.pad(9, 2);
+  minutes = this.pad(53, 2);
+  seconds = this.pad(49, 2);
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+ pad(num, size){     
+   return ('000000000' + num).substr(-size); 
   }
 
 }
